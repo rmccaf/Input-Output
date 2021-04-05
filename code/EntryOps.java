5a6ac918d273191ec62bf3b1414dc8a686c33eee
Ryan Mccaffery <rmichmccaff@gmail.com>
2021-04-05 08:49:11 -0500

    Implemented over/underflow protected addition methods

--- code/EntryOps.java
+++ code/EntryOps.java
@@ -11,25 +11,95 @@ public class EntryOps extends AbsEntryOps<NumEntry>
 {
 
 
-	public NumEntry addition(NumEntry entryOne, NumEntry entryTwo)
+	public static NumEntry addition(NumEntry entryOne, NumEntry entryTwo)
 	{
+	
+		//check for multiplication overflow				
+		if(multOverflow(entryOne.getNum(),entryTwo.getDen()))
+			return (NumEntry) null;
+
+		if(multOverflow(entryOne.getDen(),entryTwo.getDen()))
+			return (NumEntry) null; 
 		
-	
-		long numOne = entryOne.getNum() * entryTwo.getDen(); 
-		long denOne = entryOne.getDen() * entryTwo.getDen(); 
+		if(multOverflow(entryTwo.getNum(),entryOne.getDen()))
+			return (NumEntry) null;
+			
+			
+		//create common denominators
+		long commonNumOne = entryOne.getNum() * entryTwo.getDen(); 
+		long commonDen = entryOne.getDen() * entryTwo.getDen(); 
+		long commonNumTwo = entryTwo.getNum() * entryOne.getDen(); 
+		 
 		
-		return new NumEntry(numOne + entryTwo.getNum(), denOne);		
-	
-	
+		//check for addition overflow
+		if(addOverFlow(commonNumOne,commonNumTwo))
+			return (NumEntry) null; 	
+			
+		//add result numerators 
+		long numResult = commonNumOne + commonNumTwo; 
+					
+		
+		//return result
+		return new NumEntry(numResult,commonDen); 
+		 
+		
 	}//end addition 
 	
 	
-	public StringEntry adddition(StringEntry entryOne, StringEntry entryTwo)
+	public static StringEntry adddition(StringEntry entryOne, StringEntry entryTwo)
 	{
 	
 		return entryOne; 
 	
 	}//end addition
 		
+		
+		
+	public static boolean addOverFlow(long numOne, long numTwo)
+	{
+	
+		boolean returnBoolean = false; 
+		
+		if(numOne > 0 && numTwo > 0)
+		{
+		
+			if((numOne + numTwo ) <= 0)
+				returnBoolean = true; 
+		
+		}
+		else if(numOne < 0 && numTwo < 0)
+		{
+			
+			if((numOne + numTwo) >= 0)
+				returnBoolean = true; 
+		
+		}
+		
+		
+		return returnBoolean; 
+		
+	}//end overFlow 
+
+
+
+	public static boolean multOverFlow(long numOne, long numTwo)
+	{
+		
+		boolean returnBoolean = false; 
+		
+		//multiply the two numbers 
+		long result = numOne * numTwo; 
+		
+		//divide and check result 
+		result /= numTwo; 
+		
+		if(result != numOne)
+			returnBoolean = true; 
+		
+		return returnBoolean; 
+			
+	}//end multOverFlow() 
+	
+	
 
 }//end EntryOps
--- code/EntryOpsTest.java
+++ code/EntryOpsTest.java
@@ -3,10 +3,107 @@ public class EntryOpsTest
 
 	public static void main(String[] args)
 	{
-	
-
+		
+		//check numAddition() 
+				
+		
 		
 	}//end main() 
 
 
+	
+	public static File testFileBuilder(int caseNumber)
+	{
+	
+		switch(caseNumber)
+		{
+			
+			case 1: 
+				return new File("./TestCases/EntryOpsTests/AdditionCases.txt");
+		
+			default:
+				return (File) null; 
+				
+		}//end switch
+	
+	}//end fileBuilder() 
+	
+	
+		
+	public static File solFileBuilder(int caseNumber)
+	{
+	
+		switch(caseNumber)
+		{
+			
+			case 1: 
+				return new File("./TestCases/EntryOpsTests/AdditionSol.txt");
+			default:
+				return (File) null; 
+				
+		}//end switch
+	
+	}//end solFileBuilder() 
+		
+		
+		
+	public static Scanner scanBuild(File caseFile)
+	{
+	
+		try
+		{
+			
+			return new Scanner(caseFile); 	
+			
+		}
+		catch(FileNotFoundException e)
+		{
+		
+			System.out.println("There is no such file."); 
+			
+		}//end catch 
+		
+		return (Scanner) null;
+		
+	}//end scanbuild 
+	
+	
+	
+	public static ArrayList<NumEntry> caseBuild(Scanner scan)
+	{
+	
+		int numberOfCases = scan.nextInt(); 
+		
+		ArrayList<NumEntry> cases = new ArrayList<NumEntry>(); 
+		
+		for(int i = 0 ; i < numberOfCases ; i++)
+		{
+		
+			long numerator = scan.nextLong(); 
+			long denominator = scan.nextLong(); 
+			
+			cases.add(new NumEntry(numerator,denominator)); 
+		
+		
+		}//end for 
+	
+		return cases; 
+	
+	}//end caseBuild() 
+	
+	
+	
+	public static boolean numAdditionTest(ArrayList<NumEntry> testCases, ArrayList<NumEntry> solCases)
+	{
+	
+		boolean returnBoolean = false; 
+		
+		
+		return returnBoolean; 	
+	
+	}//end numAdditionTest() 
+	
+	
+	
+	
 }//end class 
