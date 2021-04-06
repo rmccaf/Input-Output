d58a3bd30f12f91ebb92397fb027281064eeec84
Ryan Mccaffery <rmichmccaff@gmail.com>
2021-04-06 05:40:30 -0500

    implemented all EntryOps with overflow checks

--- code/EntryOps.java
+++ code/EntryOps.java
@@ -6,8 +6,9 @@
 
 
 import java.lang.Math; 
+import java.math.BigInteger; 
 
-public class EntryOps extends AbsEntryOps<NumEntry>
+public class EntryOps
 {
 
 
@@ -32,7 +33,7 @@ public class EntryOps extends AbsEntryOps<NumEntry>
 		 
 		
 		//check for addition overflow
-		if(addOverFlow(commonNumOne,commonNumTwo))
+		if(addOverflow(commonNumOne,commonNumTwo))
 			return (NumEntry) null; 	
 			
 		//add result numerators 
@@ -46,16 +47,92 @@ public class EntryOps extends AbsEntryOps<NumEntry>
 	}//end addition 
 	
 	
-	public static StringEntry adddition(StringEntry entryOne, StringEntry entryTwo)
-	{
 	
-		return entryOne; 
+	public static StringEntry addition(StringEntry entryOne, StringEntry entryTwo)
+	{
+			
+		BigInteger commonNum1 = entryOne.getNum().multiply(entryTwo.getDen()); 
+		BigInteger commonNum2 = entryTwo.getNum().multiply(entryOne.getDen());
+		BigInteger commonDen  = entryTwo.getDen().multiply(entryTwo.getDen());
+				
+		return new StringEntry(commonNum1.add(commonNum2).toString(),commonDen.toString());  
 	
 	}//end addition
 		
+	
+	
+	public static NumEntry subtraction(NumEntry entryOne, NumEntry entryTwo)
+	{
+	
+		//negate entry two checking for overflow 
+		NumEntry negEntryTwo = multiplication(entryTwo,new NumEntry(-1,1));
 		
+		if(negEntryTwo == null)		
+			return (NumEntry) null; 
+			
+		//send to addition checking for overflow 
+		NumEntry result = addition(entryOne,negEntryTwo);
 		
-	public static boolean addOverFlow(long numOne, long numTwo)
+		if(result == null)
+			return (NumEntry) null; 
+		 
+		//return result
+		return result; 
+		
+	}//end subtraction() 
+	
+
+	public static StringEntry subtraction(StringEntry entryOne, StringEntry entryTwo)
+	{
+		
+		StringEntry negEntryTwo = multiplication(entryTwo,new StringEntry("-1","1"));
+		
+		return addition(entryOne,negEntryTwo);
+		
+	}//end subtraction() 	
+	
+
+	public static NumEntry multiplication(NumEntry entryOne, NumEntry entryTwo)
+	{
+	
+		//check for multiplication over flow 
+		if(multOverflow(entryOne.getNum() , entryTwo.getNum()) || multOverflow(entryOne.getDen(),entryTwo.getDen()))
+			return (NumEntry) null; 
+			
+		return new NumEntry(entryOne.getNum() * entryTwo.getNum() , entryOne.getDen() * entryTwo.getDen() ); 	
+		
+	}//end multiplication 
+		
+	
+	
+	public static StringEntry multiplication(StringEntry entryOne, StringEntry entryTwo)
+	{
+	
+		return new StringEntry(entryOne.getNum().multiply(entryTwo.getNum()).toString(),entryOne.getDen().multiply(entryTwo.getDen()).toString()); 
+	
+	}//end multiplication() 
+	
+	
+	
+	public static NumEntry division(NumEntry entryOne, NumEntry entryTwo)
+	{
+				
+		//returns null if overflow by using multiplication 
+		return multiplication(entryOne,new NumEntry(entryTwo.getDen(),entryTwo.getNum())); 
+	
+	}//end division() 
+	
+		
+	public static StringEntry division(StringEntry entryOne, StringEntry entryTwo)
+	{
+		
+		return multiplication(entryOne,new StringEntry(entryTwo.getDen().toString(),entryTwo.getNum().toString()));
+		
+	}//end division() 
+	
+	
+		
+	private static boolean addOverflow(long numOne, long numTwo)
 	{
 	
 		boolean returnBoolean = false; 
@@ -82,7 +159,7 @@ public class EntryOps extends AbsEntryOps<NumEntry>
 
 
 
-	public static boolean multOverFlow(long numOne, long numTwo)
+	private static boolean multOverflow(long numOne, long numTwo)
 	{
 		
 		boolean returnBoolean = false; 
@@ -99,6 +176,8 @@ public class EntryOps extends AbsEntryOps<NumEntry>
 		return returnBoolean; 
 			
 	}//end multOverFlow() 
+	
+	
 	
 	
 
